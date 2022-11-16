ac125ee4877980f49d9f9c6fa6a7eecdccf988d9
Jr.Josias2 <jr.josias2@gmail.com>
2022-11-15 22:56:49 -0300

    json practices handling values add, removal and replacement

--- FreeCode_Camp/Python_intermediate/class_json.py
+++ FreeCode_Camp/Python_intermediate/class_json.py
@@ -2,26 +2,34 @@
 import json
 
 person = dict(nome="Josias", idade=39, cidade="Sao Paulo", TemFilhos=False, Titulos=["Engenheiro de Dados", "GCP", "AWS", "Python", "Arquiteto de Solucoes"])
-print(person.items())
+print('Person Items:\n', person.items())
 
 #Python dict to Json
 personJSON = json.dumps(person, indent=4)
-print(personJSON)
+print('Person parsed by json.dumps:\n', personJSON)
+
+pos = person['Titulos'].index('Python')
+person['Titulos'].remove('GCP')
+person['Titulos'].append('GCP K8S')
+person['Titulos'].append('GCP Data Engineer')
+print(person.items())
 
 #Save it in json format
-with open('person.json', 'w') as file:
+with open('person_dict.json', 'w') as file:
     json.dump(person, file, indent=4)
 
-#Qdo carregado na memoria se usa sempre o loads ou dumps aonde o s significa string ou direto do stream, enquanto sem o s significa salvar, ler o arquivo fisico em disco
-person2 = json.loads(personJSON)
-print(person2)
+with open('person_str.json', 'w') as file:
+    json.dump(personJSON, file, indent=4)
 
-with open('person.json', 'r') as file:
+#Qdo carregado na memoria ou através de Arquivo Texto se usa sempre o loads ou dumps aonde o s significa string ou direto do stream, enquanto sem o s significa salvar, ler o arquivo fisico em disco
+person2 = json.loads(personJSON)
+print('Loads from personJson:\n', person2)
+
+with open('person_dict.json', 'r') as file:
     person3 = json.load(file)
-    print(person3)
+    print('File loaded:\n', person3)
 
 class User:
-
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -33,9 +41,14 @@ def encode_user(obj):
         raise TypeError('Objeto tipo User nao eh serializavel em JSON')
 
 user = User('Josa', 40)
+user2 = {'name':'Josias', 'age' : '40'}
 
 userJSON = json.dumps(user, default=encode_user)
-print(userJSON)
+print('Parsed by json.dumps:\n', userJSON)
+
+# Como nao e uma instancia da classe User, ele nao entra no metodo encode_user da classe acima
+userJSON2 = json.dumps(user2, default=encode_user)
+print('Parsed by json.dumps:\n', userJSON2)
 
 #BEM MAIS FACIL
 from json import JSONEncoder, JSONDecoder
@@ -52,8 +65,14 @@ class UserDecoder(JSONDecoder):
             return User(name=o['name'], age=o['age'])
         return o
 
-userJSON2 = UserEncoder().encode(user)
-print(userJSON2)
+userJSON = UserEncoder().encode(user)
+print("User Encoder", userJSON)
+
+userDict1 = UserDecoder().decode(userJSON)
+print("User decoder", userDict1)
+
+userJSON2 = UserEncoder().encode(user2)
+print("User Encoder 2", userJSON)
 
 userDict1 = UserDecoder().decode(userJSON2)
-print(userDict1)
+print("User decoder 2 ", userDict1)
