7503359ddce89d86358498616198620c548007ed
Jr.Josias2 <jr.josias2@gmail.com>
2022-11-17 21:10:49 -0300

    Restore folders from Abbott laptop DataWrangling for CA and PysPark for Udemy

---
+++ Cloud_Academy/Data_wrangling/apps.py
@@ -0,0 +1,11 @@
+'''
+from django.apps import AppConfig
+
+class Student(AppConfig):
+    name = 'Student_model'
+    verbose_name = 'Student ORM model'
+
+class Club(AppConfig):
+    name = 'Club_model'
+    verbose_name = 'Club ORM model'
+'''
